521e79c077d82f2a2ae569ae0a6938d8e8e1747d
stolomeo <tolomeosebastian@gmail.com>
2022-08-20 13:15:53 -0400

    Fix spelling

--- components/layout/Navigation/index.tsx
+++ components/layout/Navigation/index.tsx
@@ -41,7 +41,7 @@ export default function Navigation() {
                 <NavDropdown.Item href="restaurants">RESTAURANTS</NavDropdown.Item>
 
                 <NavDropdown.Divider />
-                <span className="mx-2 text-warning">COMMING SOON </span>
+                <span className="mx-2 text-warning">COMING SOON </span>
                 <NavDropdown.Item href="#">PARKS</NavDropdown.Item>
                 <NavDropdown.Item href="#">CAFES</NavDropdown.Item>
                 <NavDropdown.Item href="#">SHOPPING</NavDropdown.Item>
