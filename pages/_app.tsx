992804792aa33ef0ccb778919503680301d9a050
Peter Lewandowski <mapiotr2010@gmail.com>
2022-08-29 00:31:21 -0400

    initial user state should not be empty object, fixed (set to undefined)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react'
-import { useState, useEffect } from 'react'
-import type { AppProps } from 'next/app'
-import { app } from '../firebaseConfig'
-import { getAuth, onAuthStateChanged, User } from 'firebase/auth'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import * as React from "react"
+import { useState, useEffect } from "react"
+import type { AppProps } from "next/app"
+import { app } from "../firebaseConfig"
+import { getAuth, onAuthStateChanged, User } from "firebase/auth"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-import '../styles/main.scss'
+import "../styles/main.scss"
 
-import Layout from './../components/layout'
+import Layout from "./../components/layout"
 
 export interface IUser {
   id?: string | null
@@ -19,7 +19,7 @@ export interface IUser {
 export const UserContext = React.createContext({} as IUser)
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [user, setUser] = useState<IUser | undefined>({})
+  const [user, setUser] = useState<IUser | undefined>()
 
   const auth = getAuth(app)
 
