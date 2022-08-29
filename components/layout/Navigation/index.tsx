b624e6a8e08c2e7fb58e38fcfabd790684fa27f0
Peter Lewandowski <mapiotr2010@gmail.com>
2022-08-29 00:35:52 -0400

    split jsx into Tab folder components, refactored

--- components/layout/Navigation/index.tsx
+++ components/layout/Navigation/index.tsx
@@ -5,8 +5,9 @@ import styles from "./Navigation.module.scss"
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
 
 import { UserContext } from "../../../pages/_app"
-import IUser from "../../../pages/_app"
 import Login from "../../Login"
+import AroundBCTab from "./Tabs/AroundBCTab"
+import StudentsAreaTab from "./Tabs/StudentsAreaTab"
 
 export default function Navigation() {
   // @ts-ignore
@@ -36,32 +37,13 @@ export default function Navigation() {
               <Nav.Link href="news">NEWS</Nav.Link>
               <Nav.Link href="events">EVENTS</Nav.Link>
               <Nav.Link href="/#links">LINKS</Nav.Link>
-
-              <NavDropdown title="AROUND BC" id="nav-dropdown">
-                <NavDropdown.Item href="restaurants">RESTAURANTS</NavDropdown.Item>
-
-                <NavDropdown.Divider />
-                <span className="mx-2 text-warning">COMING SOON </span>
-                <NavDropdown.Item href="#">PARKS</NavDropdown.Item>
-                <NavDropdown.Item href="#">CAFES</NavDropdown.Item>
-                <NavDropdown.Item href="#">SHOPPING</NavDropdown.Item>
-              </NavDropdown>
+              <AroundBCTab />
               {user ? (
                 <Nav.Link onClick={logOutUser}>LOGOUT</Nav.Link>
               ) : (
                 <Nav.Link onClick={() => setOpenModal(true)}>LOGIN</Nav.Link>
               )}
-              <NavDropdown title="STUDENTS ONLY" id="nav-dropdown">
-                <NavDropdown.Item href="/labs">LABS</NavDropdown.Item>
-                <NavDropdown.Item href="/challenges">CODE CHALLENGES</NavDropdown.Item>
-
-                <NavDropdown.Divider />
-                <span className="mx-3 text-warning">COMING SOON </span>
-
-                <NavDropdown.Item href="#">PHOTOS</NavDropdown.Item>
-                <NavDropdown.Item href="#">HOMEWORK</NavDropdown.Item>
-                <NavDropdown.Item href="#">JOBS</NavDropdown.Item>
-              </NavDropdown>
+              <StudentsAreaTab />
             </Nav>
           </Navbar.Collapse>
         </Container>
