1303b128ae8259e806abe902b1d9728c09b96e14
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-09-27 19:04:44 -0400

    used getserversideprops to get data, tweaked header text

--- pages/news/index.js
+++ pages/news/index.js
@@ -5,22 +5,68 @@ import { Hero } from './../../components/Hero'
 import AddNews from './AddNews'
 import styles from './News.module.scss'
 import Link from 'next/link'
+import { FirebaseGet } from '../../utilities/FirebaseUtils'
 
-export default function GetNews() {
+// export default function GetNews() {
+//   const [allNews, setAllNews] = useState()
+//   const [show, setShow] = useState()
+//   const handleClose = () => setShow(false)
+//   const handleShow = () => setShow(true)
+
+//   useEffect(() => {
+//     fetch(`https://bocacode-intranet-api.web.app/news`)
+//       .then(response => response.json())
+//       .then(data => {
+//         // data = data?.sort((a, b) => b.timestamp - a.timestamp)
+//         setAllNews(data)
+//       })
+//       .catch(err => console.log(err))
+//   }, [])
+
+function NewsCard({ news }) {
+  return (
+    <div key={news.id} className={styles['news-container']}>
+      <img
+        className={styles['newsimage']}
+        src="https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg"
+        alt=""
+      ></img>
+      <div className={styles['news-card-container']}>
+        {/* <div className={[styles['news-card-container'], styles['slideMe']]}> */}
+        <div className={styles['news-card']}>
+          <div className={styles['news-title-date-container']}>
+            <div className={styles['news-title-container']}>
+              <h1 id={styles['news_header2']}>{news.title}</h1>
+            </div>
+            <div className={styles['news-inner-triangle']}>
+              <div className={styles['news-date']}>
+                {/* {new Date(news.createdAt._seconds * 1000).toLocaleDateString()} */}
+                {new Date(news.createdAt).toLocaleDateString()}
+              </div>
+            </div>
+          </div>
+
+          <div className={styles['news-card-header']}>
+            <h2 id={styles['news_header3']}>{news.author}</h2>
+
+            <p className={styles['news-text']}>{news.body}</p>
+          </div>
+          <footer className={styles['news-card-footer']}>
+            <Link href={`/news/[id].js}`} className={styles['news-click']} key={news.id}>
+              <p>Read more</p>
+            </Link>
+          </footer>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default function News({ data }) {
   const [allNews, setAllNews] = useState()
   const [show, setShow] = useState()
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
-
-  useEffect(() => {
-    fetch(`https://bocacode-intranet-api.web.app/news`)
-      .then(response => response.json())
-      .then(data => {
-        // data = data?.sort((a, b) => b.timestamp - a.timestamp)
-        setAllNews(data)
-      })
-      .catch(err => console.log(err))
-  }, [])
 
   return (
     <>
@@ -36,9 +82,8 @@ export default function GetNews() {
           <h1 className="add-article-header">
             There is always some interesting news to share at Boca Code!
           </h1>
-          <p className="add-article-text">
-            Feel free to browse articles written by our students, alumni and instructors. To
-            contribute, go ahead and add your own article.{' '}
+          <p className={styles['add-article-text']}>
+            Feel free to browse articles written by our students, alumni and instructors.{' '}
           </p>
           <Button className={styles['btn-add-article']} variant="primary" onClick={handleShow}>
             Add New Article
@@ -46,84 +91,12 @@ export default function GetNews() {
         </div>
         <hr />
         <div>
-          {!allNews ? (
+          {/* {!allNews ? ( */}
+
+          {!data ? (
             <h2 id={styles['news_header2']}>Loading...</h2>
           ) : (
-            <div>
-              <>
-                {allNews?.map(news => {
-                  return (
-                    // <Link href={`/news/[id].js}`} className={styles['news-click']} key={news.id}>
-                    <div key={news.id} className={styles['news-container']}>
-                      {/* Placeholder image */}
-                      <img
-                        className={styles[('news-image', 'slide-in')]}
-                        src="https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg"
-                        alt=""
-                      ></img>
-
-                      {/* <Card className={styles['news-card']}>
-                          <h2 id={styles['news_header2']}>{news.title}</h2>
-                          <h3 id={styles['news_header3']}>{news.author}</h3>
-                          <h6 id={styles['news_header6']}>
-                            {new Date(news.createdAt._seconds * 1000).toLocaleDateString()}
-                          </h6>
-                          <p className={styles['news-text']}>{news.body}</p>
-                        </Card> */}
-
-                      <div className={styles[('news-card-container', 'slide-in')]}>
-                        <div className={styles['news-card']}>
-                          <div className={styles['news-title-date-container']}>
-                            <div className={styles['news-title-container']}>
-                              <h1 id={styles['news_header2']}>{news.title}</h1>
-                            </div>
-                            <div className={styles['news-inner-triangle']}>
-                              <div className={styles['news-date']}>
-                                {new Date(news.createdAt._seconds * 1000).toLocaleDateString()}
-                              </div>
-                            </div>
-                          </div>
-
-                          <div className={styles['news-card-header']}>
-                            {/* <h1 id={styles['news_header2']}>{news.title}</h1> */}
-                            <h2 id={styles['news_header3']}>{news.author}</h2>
-                            {/* <h6 id={styles['news_header6']}>
-                              {new Date(news.createdAt._seconds * 1000).toLocaleDateString()}
-                            </h6> */}
-                            <p className={styles['news-text']}>{news.body}</p>
-                          </div>
-                          <footer className={styles['news-card-footer']}>
-                            <Link
-                              href={`/news/[id].js}`}
-                              className={styles['news-click']}
-                              key={news.id}
-                            >
-                              <p>Read more</p>
-                            </Link>
-                            {/* <div className={styles['arrow-container']}>
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                class="bi bi-arrow-right"
-                                viewBox="0 0 16 16"
-                              >
-                                <path
-                                  fill-rule="evenodd"
-                                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                                />
-                              </svg>
-                            </div> */}
-                          </footer>
-                        </div>
-                      </div>
-                    </div>
-                    /* </Link> */
-                  )
-                })}
-              </>
-            </div>
+            data?.map(news => <NewsCard key={news.id} news={news} />)
           )}
         </div>
       </section>
@@ -138,4 +111,10 @@ export default function GetNews() {
       </Modal>
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const data = await FirebaseGet('news')
+  console.log({ data })
+  return { props: { data } }
 }
--- utilities/FirebaseUtils.tsx
+++ utilities/FirebaseUtils.tsx
@@ -1,11 +1,11 @@
-import { firestore } from "../firebaseConfig"
-import { collection, addDoc, getDocs } from "firebase/firestore"
+import { firestore } from '../firebaseConfig'
+import { collection, addDoc, getDocs } from 'firebase/firestore'
 
 export const FirestorePost = async (collectionName: String, newData: Object) => {
   try {
     // @ts-ignore
     const docRef = await addDoc(collection(firestore, collectionName), newData)
-    console.log("Document written with ID: ", docRef.id)
+    console.log('Document written with ID: ', docRef.id)
   } catch (err) {
     throw err
   }
@@ -18,7 +18,12 @@ export const FirebaseGet = async (collectionName: String) => {
     const data: Object[] = []
 
     querySnapshot.forEach(doc => {
-      data.push(doc.data())
+      // data.push(doc.data())
+      let thisDoc = doc.data()
+      if (thisDoc.createdAt) {
+        thisDoc.createdAt = thisDoc.createdAt.seconds * 1000
+      }
+      data.push(thisDoc)
     })
     return data
   } catch (err) {
